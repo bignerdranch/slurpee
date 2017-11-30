1999f5976280d5300a1c341490d34e9b8d58a45d
markd2 <markd@bignerdranch.com>
2017-11-30 09:32:13 -0500

    optimizes file metadata attrbiute accessing

--- slurpee/File.swift
+++ slurpee/File.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-enum FileType {
+enum FileType: Int {
     case file
     case directory
     case symbolicLink
@@ -24,27 +24,59 @@ enum FileType {
             self = .unknown
         }
     }
+    
+    init(resourceType: URLFileResourceType) {
+        switch resourceType {
+        case .namedPipe: self = .unknown
+        case .characterSpecial, .blockSpecial: self = .device
+        case .directory: self = .directory
+        case .regular: self = .file
+        case .symbolicLink: self = .symbolicLink
+        case .socket: self = .unknown
+        case .unknown: self = .unknown
+        default:
+            print("unknown unexpected resource type \(resourceType)")
+            self = .unknown
+        }
+        
+    }
 }
 
 
 /// Represents an entity in the file system
 /// This could have fun stuff like size, various flavors of checksum, 
 /// is it a directory, etc.
+public var sizeCount = 0
+public var typeCount = 0
+
 struct File {
     /// Where in the filesystem this particular file lives.
     let url: URL
 
     var fileSize: Int? {
-        return try? FileManager.default.attributesOfItem(atPath: url.path)[FileAttributeKey.size] as? Int ?? 0
+        sizeCount += 1
+        //        return try? FileManager.default.attributesOfItem(atPath: url.path)[FileAttributeKey.size] as? Int ?? 0
+        let values = try! url.resourceValues(forKeys: [.fileSizeKey])
+        let size = values.fileSize ?? 0
+        return size
     }
+    
 
     var fileType: FileType {
-        let type = ((try? FileManager.default.attributesOfItem(atPath: url.path)[FileAttributeKey.type] as? String) ?? "") ?? ""
-        return FileType(type: type)
+        typeCount += 1
+//        let type = ((try? FileManager.default.attributesOfItem(atPath: url.path)[FileAttributeKey.type] as? String) ?? "") ?? ""
+//        return FileType(type: type)
+
+        let values = try! url.resourceValues(forKeys: [.fileResourceTypeKey])
+        guard let type = values.fileResourceType else { return .unknown }
+        return FileType(resourceType: type)
     }
 
     init(url: URL) {
         self.url = url
+
+//        self.fileType = ((try? FileManager.default.attributesOfItem(atPath: url.path)[FileAttributeKey.type] as? String) ?? "") ?? ""
+//        self.fileSize = try? FileManager.default.attributesOfItem(atPath: url.path)[FileAttributeKey.size] as? Int ?? 0
     }
 }
 
@@ -62,7 +94,7 @@ extension FileManager {
     func filesInDirectory(at url: URL) -> [File] {
         do {
             let urls = try self.contentsOfDirectory(at: url,
-                                                    includingPropertiesForKeys: nil,
+                                                    includingPropertiesForKeys: [.fileSizeKey, .fileResourceTypeKey],
                                                     options: [])
             let files = urls.map(File.init(url:))
             return files
@@ -91,14 +123,46 @@ extension FileManager {
         return size
     }
     
-    // total size for a flat directory
-    func sizeOfDirectory(at url: URL) -> Int {
-        let files = self.filesInDirectory(at: url)
+    func totalSizeOfDirectoryWithDeepRecursion(at url: URL) -> Int {
+        let directoryEnumerator = self.enumerator(at: url, includingPropertiesForKeys: [.fileSizeKey, .fileResourceTypeKey]) 
         
-        let size = files.reduce(0, { accumulator, file in accumulator + (file.fileSize ?? 0) })
-        print("size! \(size)")
-        return size
+        var totalSize = 0
+        for something in directoryEnumerator! {
+            guard let url = something as? URL else {
+                continue
+            }
+            let values = try! url.resourceValues(forKeys: [.fileSizeKey, .fileResourceTypeKey])
+            let size = values.fileSize ?? 0
+            totalSize += size
+            
+            _ = File(url: url)
+            
+//            let file = File(url: url)  /// 1.9 sec
+//            if file.fileType != .directory {
+//                totalSize += file.fileSize ?? 0
+//            }
+//        
+//            let fileAttributesRaw = directoryEnumerator!.fileAttributes
+//            let directoryAttributesRaw = directoryEnumerator!.directoryAttributes
+//            guard let fileAttributes = directoryEnumerator!.fileAttributes else {
+//                continue
+//            }
+//            print("HUH?")
+//            let size = fileAttributes[FileAttributeKey.size] as? Int ?? 0
+//            totalSize += size
+        }
+
+        return totalSize
     }
+    
+//    // total size for a flat directory
+//    func sizeOfDirectory(at url: URL) -> Int {
+//        let files = self.filesInDirectory(at: url)
+//        
+//        let size = files.reduce(0, { accumulator, file in accumulator + (file.fileSize ?? 0) })
+//        print("size! \(size)")
+//        return size
+//    }
 }
 
 
--- slurpee/main.swift
+++ slurpee/main.swift
@@ -1,13 +1,61 @@
+/* How to discover
+
+how to tell if things are different. How things are different
+(ordered from most efficient to least)
+
+File struct:
+  - size
+  - url
+  - (any other metadat we want)
+
+- file size
+
+- file content checksum of a subset
+  - first K, last K, then couple of K in the middle.
+  
+- file content checksum of entire contents
+   CRC, MD5, or just sum up all the bytes, or something
+
+
+--- is there a fast way to get a file type
+  - mp3 vs jpeg vs MSWord document
+- a way of disambiguating files
+
+
+NSDocument?  Has a duplicat-detection method? (maybe related to icloud)
+
+
+partition the Files in groups of similar files - "Dupe collection"
+  - group same-y things
+  - gonna be a lot of them
+  
+Equivalence Classes?
+  - partitionng the set of Files into groups of "the same file"
+  - ulimately, get a "hey, here's all the kind-of files"
+
+flatmap
+   - removes nils
+   - "hoists" collections
+   [[[a, b, nil, c], [d, e]]][f, nil, g]
+   [a, b, c, d, e, f, g]
+
+*/
+
+
 import Foundation
+import Darwin
 
 let fm = FileManager.default
 let home = fm.homeDirectoryForCurrentUser
 
 //let dev = URL(string: "file:///dev")!
 // `File`s of everything in the home directory
-let files = fm.filesInDirectory(at: home)
+// let downloads = home.appendingPathComponent("Projects").appendingPathComponent("swift").appendingPathComponent("swift")
+// let downloads = home.appendingPathComponent("Projects")
+let downloads = home
+
+
 //let files = fm.filesInDirectory(at: dev)
-var totalFileSize: Double = 0
 
 //
 // Aggregate file sizes:
@@ -24,16 +72,31 @@ var totalFileSize: Double = 0
 //    - Con: more complex
 
 
+let startTime = mach_absolute_time()
+
+let files = fm.filesInDirectory(at: downloads)
+var totalFileSize: Int = 0
 
 for file in files {
     if file.fileType == .directory {
         print("Looking at \(file.url.lastPathComponent)...")
         let size = fm.totalSizeOfDirectory(at: file.url)
-        print("    \(file.url.lastPathComponent) TOTAL size is \(size)")
+//        let size = fm.totalSizeOfDirectoryWithDeepRecursion(at: file.url)
+        totalFileSize += size
+        print("processed \(file) : \(size)")
     }
-    print("\(file)")
 }
 
+let stopTime = mach_absolute_time()
+
+var timebase = mach_timebase_info()
+_ = mach_timebase_info(&timebase)
+
+let time = ((stopTime - startTime) * UInt64(timebase.numer)) / UInt64(timebase.denom)
+let timeInSeconds = Double(time) / 1_000_000_000
+
+print("total: \(totalFileSize / (1024 * 1024)) and took \(timeInSeconds) seconds")
+print("counts: \(sizeCount) and \(typeCount)")
 
 
 
